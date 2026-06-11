6402a52ed1d04779608311750a25fb4b1f737811
VirxEC <virx@virxcase.dev>
2026-06-10 22:00:39 -0500

    Remove `flat.rs` formatting

--- build.rs
+++ build.rs
@@ -1,50 +1,8 @@
-use std::{
-    env, fs,
-    io::Write,
-    path::PathBuf,
-    process::{Command, Stdio},
-};
+use std::{env, fs, path::PathBuf};
 
 const SCHEMA_DIR: &str = "spec";
 const ROOT_SCHEMA: &str = "core.fbs";
 const OUT_FILE: &str = "flat.rs";
-
-fn format_string(s: &str) -> Result<String, String> {
-    let mut child = Command::new("rustfmt");
-
-    child
-        .arg("--edition=2024")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped());
-
-    let mut child = child
-        .spawn()
-        .map_err(|err| format!("Unable to spawn rustfmt. Perhaps it is not installed? {err}"))?;
-
-    child
-        .stdin
-        .as_mut()
-        .unwrap()
-        .write_all(s.as_bytes())
-        .map_err(|err| format!("Unable to write generated code to rustfmt: {err}"))?;
-
-    let output = child
-        .wait_with_output()
-        .map_err(|err| format!("Unable to get formatted code back from rustfmt: {err}"))?;
-
-    if output.status.success() && output.stderr.is_empty() {
-        Ok(String::from_utf8_lossy(&output.stdout).into_owned())
-    } else if output.stderr.is_empty() {
-        Err(format!("rustfmt failed with exit code {}", output.status))
-    } else {
-        Err(format!(
-            "rustfmt failed with exit code {} and message:\n{}",
-            output.status,
-            String::from_utf8_lossy(&output.stderr),
-        ))
-    }
-}
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -55,8 +13,7 @@ fn main() {
     let raw_out = planus_codegen::generate_rust(&declarations, false)
         .unwrap()
         .replace("#[no_implicit_prelude]\n", "");
-    let formatted_out = format_string(&raw_out).unwrap();
 
     let out_path = PathBuf::from(env::var_os("OUT_DIR").unwrap()).join(OUT_FILE);
-    fs::write(out_path, formatted_out.as_bytes()).unwrap();
+    fs::write(out_path, raw_out.as_bytes()).unwrap();
 }
